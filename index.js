e92a1649e7b15ca910678b4be3a2758531572d44
olivia-russell <olivia.posey7@gmail.com>
2023-08-23 21:09:02 -0500

    adding displayBooks
    adding displayBooks

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let library = [];
 
 
+
 // Constructor for the book
 function Book (title, author, pages, status) {
         this.title = title;
@@ -12,9 +13,36 @@ function Book (title, author, pages, status) {
 
 // Adding a new book
 function addBook (title, author, pages, status) {
-    let book = new Book(title, author, pages, status)
-    library.push(book);ddd
+    let book = new Book(title, author, pages, status);
+    library.push(book);
 }
+
+// Display books on page
+
+function displayBooks() {
+    const books = document.querySelector(".books");
+    console.log(books);
+
+    // loop over library array
+    library.forEach(library => {
+        const card = document.createElement("div");
+        console.log(card);
+        card.classList.add("card");
+        books.appendChild(card);
+        for(let key in library) {
+            console.log(`${key}: ${library[key]}`);
+            const para = document.createElement("p");
+            para.textContent = (`${key}: ${library[key]}`);
+            card.appendChild(para);
+
+        }
+    })
+        
+}
+
+addBook('A title', 'An author', "321", "true");
+addBook('Title two', 'Author two', '534', 'false');
+displayBooks();
 
 
 
